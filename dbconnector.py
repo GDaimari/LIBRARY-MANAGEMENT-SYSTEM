dbdd4037ae86c37dacd2e3d6b8abdd94aa025ee6
Giteeka Daimari <giteekadaiamri@gmail.com>
2025-05-22 23:34:12 +0530

    Re-add cleaned dbconnector.py with env variables

--- dbconnector.py
+++ dbconnector.py
@@ -1,13 +1,16 @@
-                        # Database connection
+# Database connection
 
 import mysql.connector
+import os
 
 def connect_db():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="#daimari123",
-        database="LBMS")
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
+
 
 
 
